259df41fdb3ca6fc1a6ce3d66d4839d1da5c75c3
Fabio Zheng <zheng.fabio@iisgalvanimi.edu.it>
2024-11-20 09:47:09 +0000

    metodo rimuovi aggiunta

--- spesa.py
+++ spesa.py
@@ -1,7 +1,10 @@
 lista_della_spesa = []
 def aggiungi():
-    lista.append(valore)
+    lista_della_spesa.append(valore)
 
 def visualizza():
     for i in range(len(lista_della_spesa)):
     print(f"{i + 1}. { lista_della_spesa[i]}")
+
+def rimuovi():
+    lista_della_spesa.pop(valore)
